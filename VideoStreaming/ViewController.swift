ab39e634eff8346052b51a2225b16736c704ffa4
torinunna <torinunna9588@gmail.com>
2024-04-12 18:24:40 +0900

    Initial Commit

---
+++ VideoStreaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoStreaming
+//
+//  Created by YUJIN KWON on 4/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
